f0c82071da781a6b614267ff276c639b1b723b72
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-05 21:52:40 -0400

    created controller static_pages with home, about, contact, and faq actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact_us'
+
+  get 'static_pages/faq'
+
   get 'pages/index'
 
   resources :events
