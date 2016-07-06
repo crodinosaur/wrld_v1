7d0f7b681e140099408d58499190cc90b2483b84
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-06 13:38:52 -0400

    updated user_facts_controller with update and strong params actions

--- app/controllers/user_facts_controller.rb
+++ app/controllers/user_facts_controller.rb
@@ -1,4 +1,14 @@
 class UserFactsController < ApplicationController
 	def edit
 	end
+
+	def update
+		@user = current_user
+		@user.update_attributes(user_fact_params)
+		render 'edit'
+	end
+
+	def user_fact_params
+		params.require(:user).permit(:first_name, :last_name, :date_of_birth, :state, :zip, :country, :gender, :veteran, :bio, :picture)
+	end
 end
