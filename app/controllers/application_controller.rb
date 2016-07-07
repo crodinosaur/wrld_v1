4a8811394fa823f53b8e3fc642b263ba132cacbb
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-07 14:36:12 -0400

    http auth works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_auth_with(name: ENV['site_name'], password: ENV['password']) if Rails.env.production?
+  http_basic_authenticate_with(name: ENV['site_name'], password: ENV['password']) if Rails.env.production?
 
   private
   
