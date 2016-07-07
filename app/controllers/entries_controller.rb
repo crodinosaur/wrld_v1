0b75e2906d7a76680775974ceaef9beb5d08488a
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-07 13:40:54 -0400

    updated event and entries view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -73,6 +73,7 @@ class EntriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_entry
       @entry = Entry.find(params[:id])
+      @event = Event.find(params[:event_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
