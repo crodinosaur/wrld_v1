a9841ddcf128c1879466c7525b5f7c1532fa9b62
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-12 13:19:21 -0400

    added authenticate_user to events and entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,9 @@
 class EntriesController < ApplicationController
 
   #->Prelang (scaffolding:rails/scope_to_user)
-  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+  # before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
+  before_action :authenticate_user!
 
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
 
