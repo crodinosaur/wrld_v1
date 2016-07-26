c72ba80b83f218c693a1f4e69d4c2ffc67a73f69
Dino Bilankov <dino.bilankov@gmail.com>
2016-07-26 11:36:27 -0400

    edited user admin pages

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,35 @@
 ActiveAdmin.register User do
 
+index do
+    column :first_name
+    column :last_name
+    column :email
+    column :dob 
+    column :gender
+    column :veteran
+    column :country
+	column :state
+    column :zip
+    column :bio
+    actions
+end
+
+form do |f|
+	inputs 'New User' do
+		input :email
+	    input :picture
+	    input :first_name
+	    input :last_name
+	    input :dob
+	    input :gender
+	    input :veteran
+	    input :country
+		input :state
+	    input :zip
+	    input :bio
+	end
+	actions
+end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
