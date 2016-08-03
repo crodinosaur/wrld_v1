42ce54fb40f45e97c1fc484532bd47f16e68f59e
Dino Bilankov <dino.bilankov@gmail.com>
2016-08-03 13:40:03 -0400

    invitation mailer start

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,12 @@
 class Invitation < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+
+  # before_create :send_out_code
+
+
+  # def send_out_code
+  # 	InvitationMailer.send_to_follower(self)
+  # end
+
 end
